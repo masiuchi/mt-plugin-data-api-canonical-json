2ec212977cb26689b5796fb8eb35f19b54c1ad82
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2015-02-13 08:32:12 +0000

    Version 0.01.

---
+++ plugins/DataAPICanonicalJSON/DataAPICanonicalJSON.pl
@@ -0,0 +1,39 @@
+package MT::Plugin::DataAPICanonicalJSON;
+use strict;
+use warnings;
+use utf8;
+
+use base qw( MT::Plugin );
+
+my $plugin = __PACKAGE__->new(
+    {   name        => 'DataAPICanonicalJSON',
+        version     => 0.01,
+        author_name => 'Masahiro Iuchi',
+        author_link => 'https://github.com/masiuchi',
+        plugin_link =>
+            'https://github.com/masiuchi/mt-plugin-data-api-canonical-json',
+        description =>
+            '<__trans phrase="The same object should always be stringified to the exact same string.">',
+    }
+);
+MT->add_plugin($plugin);
+
+{
+    require MT::DataAPI::Format::JSON;
+    my $serialize = \&MT::DataAPI::Format::JSON::serialize;
+
+    require MT::Util;
+    my $to_json = \&MT::Util::to_json;
+
+    no warnings 'redefine';
+    *MT::DataAPI::Format::JSON::serialize = sub {
+        local *MT::Util::to_json = sub {
+            my ( $value, $args ) = @_;
+            $args->{canonical} = 1;
+            $to_json->( $value, $args );
+        };
+        $serialize->(@_);
+    };
+}
+
+1;
